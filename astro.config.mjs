ef322274682365f0bb96212068d17d8660133603
paulk2001 <paul.finn.koch@gmail.com>
2025-05-21 12:03:45 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@astrojs/react";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "",
+      },
+      bridge: {
+        customParent: "https://app.storyblok.com",
+      },
+      components: {
+        page: "storyblok/Page",
+        Project: "storyblok/Project",
+        Hero: "storyblok/Hero",
+        CvEntry: "storyblok/CvEntry",
+        CvSection: "storyblok/CvSection",
+        TextBlock: "storyblok/TextBlock",
+        TwoColumnBlock: "storyblok/TwoColumnBlock",
+        ImageBlock: "storyblok/ImageBlock",
+        VideoBlock: "storyblok/VideoBlock",
+      },
+    }),
+    tailwind(),
+    react(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
